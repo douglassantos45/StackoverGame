9d87485c71564a168ec781ce3200485f7eba7b67
Douglas Silva <douglasifbaianosb@gmail.com>
2022-11-07 16:00:54 -0300

    feat: add router to challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { Challenges } from './layouts/Challenges';
 import { Home } from './layouts/Home';
 import { Login } from './layouts/Login';
+import { Ranking } from './layouts/Ranking';
 
 const App = () => {
-  return <Home />;
+  return (
+    <Login />
+  );
 };
 
 export default App;
