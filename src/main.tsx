095533e2418f09912d37e3f0f1b6eca2ba50766c
Douglas Silva <douglasifbaianosb@gmail.com>
2022-11-07 16:02:58 -0300

    refactor: remove components

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'
+import './index.css';
 import { Router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
